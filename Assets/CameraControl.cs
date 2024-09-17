0db974b4e8b6ba24f6cfdff69909cf1eb76845fb
Cleverland197710 <realzacharyjayden@gmail.com>
2024-09-17 15:34:21 +0100

    1st push

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    // Create a public refrence to the player - we will asign this using the unity editor
+    public GameObject player;
+
+    void Update()
+    {
+        // Change our position relative to the players positon
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+
+    }
+}
