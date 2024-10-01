3b264d7e337443ccc546ae9e855014d9ef933866
Cleverland197710 <realzacharyjayden@gmail.com>
2024-10-01 10:46:16 +0100

    01.10.24
    yh

--- Assets/CoinSpawner.cs
+++ Assets/CoinSpawner.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CoinSpawner : MonoBehaviour
 {
-    // Refrence to the  
+    // Refrence to the coin prefab
+    public GameObject coinPrefab;
+
+    // Define the offset or range where the coins should spawn on the platform
+    public Vector2 coinOffset = new Vector2(0, 1); //Adjust as necessary
+
     void Start()
     {
+        // Spawn the coin on the platform at the desired position
+        SpawnCoin();
         
     }
 
     // Update is called once per frame
-    void Update()
+    void SpawnCoin()
     {
+        // Calculate the coin position relative ro the platform
+        Vector3 coinPosition = transform.position + (Vector3)coinOffset;
+
+        //Instantiate the coin at the calculated position
+        Instantiate(coinPrefab, coinPosition, Quaternion.identity);
         
     }
 }
