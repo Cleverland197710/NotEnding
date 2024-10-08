29d801ee7c4172ea6ba16d3ee632237ade93f38b
Cleverland197710 <realzacharyjayden@gmail.com>
2024-10-08 15:44:48 +0100

    Last period
    Speed up & Slow down

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,11 +13,12 @@ public class PlayerMovement : MonoBehaviour
     public AudioSource musicPlayer;
 
     public float movespeed = 1f;            // Constant forward speed
+    private float newMoveSpeed;
     public float jumpforce = 10f;           // Jump force
     public Transform groundCheckPoint;     // A point to check if the player is grounded
     public float checkRadius = 0.2f;       // Radius of the overlap circle for ground detection
     public LayerMask groundLayer;          // Layer of the ground objects
-    public float gravity = 4.5f;
+    public float gravity = 1f;
 
     public bool doubleJump;
 
@@ -35,7 +36,10 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();  // Get the Rigidbody2D component attached to the player
         hopSfx = GetComponent<AudioSource>();
         //anim = GetComponent<Animator>();
+
+        newMoveSpeed = movespeed;
     }
+
 
     void Update()
     {
@@ -58,9 +62,35 @@ public class PlayerMovement : MonoBehaviour
             jump();
         }
 
-        if (isGrounded = false && Input.GetKeyDown(KeyCode.Space))
+
+        if (isGrounded == false && Input.GetKeyDown(KeyCode.DownArrow))
         {
             fall();        
+        }
+
+        if (isGrounded == true)
+        {
+            resetGrav();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            slow();
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            stopSlow();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            fast();
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            stopFast();
         }
 
         /*
@@ -91,7 +121,33 @@ public class PlayerMovement : MonoBehaviour
     private void fall()
     {
         // Add an upward force for jumping
-        rb.gravityScale = (gravity * 10);
+        rb.gravityScale = (gravity * 3);
+    }
+
+    private void resetGrav()
+    {
+        rb.gravityScale = (gravity);
+    }
+
+    private void slow()
+    {
+        movespeed *= 0.5f;
+    }
+
+    private void stopSlow()
+    {
+        movespeed = newMoveSpeed;
+    }
+
+    private void fast()
+    {
+        movespeed *= 2f;
+        //movespeed = (movespeed * 5f);
+    }
+
+    private void stopFast()
+    {
+        movespeed = newMoveSpeed;
     }
 
 
